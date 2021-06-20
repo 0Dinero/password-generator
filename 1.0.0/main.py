095c802894a555f467b016a7845117c32f8fb3be
0Dinero <81997618+0Dinero@users.noreply.github.com>
2021-06-20 21:25:28 +0200

    1.0.0

---
+++ 1.0.0/main.py
@@ -0,0 +1,24 @@
+import time
+from random import randint
+from random import seed
+def randomgen(num):
+    counter = 1
+    password = ""
+    letters = ["A","b","C","d","E","f","G","h","!","12","1","2","3","4","5","6","7","8","9","10"]
+    while counter <= num:
+        time.sleep(0.1)
+        seed()
+        x = randint(0, 19)
+        i = letters[x]
+        xx = str(i)
+        password += xx
+        counter +=1
+    print(password)
+print("Welcome to my password-generator")
+i = input("How many characters do you want your password to contain?: ")
+ii = int(i)
+if ii <= 0:
+    exit()
+else:
+    print("Please wait a few seconds while we generate your password...")
+    randomgen(ii)
---
+++ readme.txt
@@ -0,0 +1,2 @@
+i created this password-generator for myself but i decided that i wanna give it to you guys.
+Go ahead and download it and see if you can learn anything from the source-code.
